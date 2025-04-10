40d1d4c3fbcc747b759162bdf3122c72416c17fa
Jimmy0016 <jrn.espinoza@yavirac.edu.ec>
2025-04-10 09:22:40 -0500

    customerssss

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,13 +1,40 @@
-import { Controller,Get,HttpStatus,Param,Res} from '@nestjs/common';
+import { Controller,Get,HttpStatus,Param,Res,Put,Body,Delete,HttpCode} from '@nestjs/common';
+import { Customer } from './interface/customer/customer.interface';
+import { CustomersService } from './customers.service';
 
 @Controller('customers')
 export class CustomersController {
-    @Get(':id')
-    find(@Res() response, @Param('id') id: number) {
-      if(id < 100) {
-        return response.status(HttpStatus.OK).send(`Página del producto ${id}`);
-      } else {
-        return response.status(HttpStatus.NOT_FOUND).send(`Producto inexistente`);
+  constructor(private readonly customersService: CustomersService) { }
+      @Get()
+      getAllProducts(): Customer[] {
+        return this.customersService.getCust();
       }
-    }
+    
+      @Get(':id')
+      find(@Param('id') id: number) {
+        return this.customersService.getId(id);
+      }
+       @Put(':id')
+          update(
+            @Param('id') id: number, 
+            @Body() body,
+          ) {
+            return this.customersService.update(id, body);
+          }
+          @Delete(':id')
+              @HttpCode(HttpStatus.NO_CONTENT)
+              delete(@Param('id') id: number) {
+                this.customersService.delete(id);
+              }
+
+
+  
+    // @Get(':id')
+    // find(@Res() response, @Param('id') id: number) {
+    //   if(id < 100) {
+    //     return response.status(HttpStatus.OK).send(`Página del producto ${id}`);
+    //   } else {
+    //     return response.status(HttpStatus.NOT_FOUND).send(`Producto inexistente`);
+    //   }
+    // }
 }
---
+++ src/customers/customers.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { Customer } from './interface/customer/customer.interface';
+@Injectable()
+export class CustomersService  { 
+    private custmon: Customer[] = [
+    {
+        id: 1,
+        name: 'Juan',
+        age: 20,
+        birthday: new Date('2005/4/24'),
+      },
+      {
+        id: 2,
+        name: 'Mario',
+        age: 19,
+        birthday: new Date('2006/4/8'),
+      },];
+      
+ getCust():Customer [] {
+  return this.custmon;
+ }
+            
+getId(id: number): Customer | undefined {
+ return this.custmon.find( (item: Customer) => item.id == id);
+ }
+ update(id: number, body: any) {
+           let CUSTOMER: Customer = {
+             id,
+             name: body.name,
+             birthday: body.birthday,
+             age: body.age
+           }
+           this.custmon = this.custmon.map( (item: Customer) => {
+             console.log(item, id, item.id == id);
+             return item.id == id ? CUSTOMER : item;
+           });
+         }
+         delete(id: number) {
+                   this.custmon = this.custmon.filter( (item: Customer) => item.id != id );
+                 }
+                 private lastId(): number {
+                    return this.custmon[this.custmon.length - 1].id;
+                  }
+    
+}
